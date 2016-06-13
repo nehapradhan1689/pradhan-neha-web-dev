b80f9a08914a8b3f7c41412eb1bf8f02037460c0
nehapradhan1689 <pradhan.n@husky.neu.edu>
2016-06-12 23:40:51 -0400

    Changed DB connection string

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,10 +1,19 @@
 //Entry point for interacting with the DB
 module.exports = function() {
 
-    var mongoose = require('mongoose');
-    //This connects to the DB cs5610summer1, if this DB doesn't exist it creates a new one with the same name.
-    mongoose.connect('mongodb://localhost/cs5610summer1');
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+    
     var models = {
         userModel: require("./user/user.model.server")(),
         websiteModel: require("./website/website.model.server")(),
